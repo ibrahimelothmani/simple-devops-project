573686fa509ede3fa31f7de56c77a55f32fcbe36
Ibrahim El Othmani <141041774+ibrahimelothmani@users.noreply.github.com>
2026-02-28 16:56:21 +0100

    build: Install initial frontend and backend project dependencies.

---
+++ infrastructure/db.sql
@@ -0,0 +1,5 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY, -- Auto-incrementing ID
+  name VARCHAR(100) NOT NULL, -- User name, required
+  email VARCHAR(250) NOT NULL -- User email, required
+);
